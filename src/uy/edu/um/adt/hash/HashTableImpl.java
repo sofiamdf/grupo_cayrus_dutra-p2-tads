343fe09b76c1ace792e330af06454f2c5bd7f049
Sofia Dutra <smdutra@correo.um.edu.uy>
2024-05-20 13:24:48 -0300

    hash table commit

---
+++ src/uy/edu/um/adt/hash/HashTableImpl.java
@@ -0,0 +1,19 @@
+package uy.edu.um.adt.hash;
+
+public class HashTableImpl implements HashTable<K, V> {
+
+    @Override
+    public void put(K key, V value) {
+
+    }
+
+    @Override
+    public boolean contains(K key) {
+        return false;
+    }
+
+    @Override
+    public void remove(K key) {
+
+    }
+}
